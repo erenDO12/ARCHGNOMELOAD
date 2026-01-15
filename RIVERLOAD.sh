b0719a575f8131fd434cbf3071cbd019d9faf6b0
erenDO12 <166924100+erenDO12@users.noreply.github.com>
2026-01-15 22:24:28 +0300

    Update RIVERLOAD.sh

--- RIVERLOAD.sh
+++ RIVERLOAD.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 
 TITLE="Arch River Installer"
+LOGFILE="install.log"
 
-# Disk seçimi
+# Başlangıç temizliği
+umount -R /mnt 2>/dev/null
+mkdir -p /mnt
+
+# Gauge ile kurulum süreci
+(
+echo 5; echo "Disk seçimi yapılıyor..."
 DISKS=$(lsblk -dpno NAME | grep -E "/dev/sd|/dev/nvme")
 MENU_OPTS=()
 for d in $DISKS; do
   MENU_OPTS+=("$d" "$d")
 done
-
 DISK=$(whiptail --title "$TITLE" --menu "Hedef Disk Seçin:" 20 60 10 "${MENU_OPTS[@]}" 3>&1 1>&2 2>&3)
 
-# Kullanıcı bilgileri
 NEWUSER=$(whiptail --title "$TITLE" --inputbox "Yeni kullanıcı adı:" 10 50 3>&1 1>&2 2>&3)
 USERPASS=$(whiptail --title "$TITLE" --passwordbox "Kullanıcı şifresi:" 10 50 3>&1 1>&2 2>&3)
 ROOTPASS=$(whiptail --title "$TITLE" --passwordbox "Root şifresi:" 10 50 3>&1 1>&2 2>&3)
 
-# Özet ekranı
-whiptail --title "$TITLE" --msgbox "Disk: $DISK\nKullanıcı: $NEWUSER" 10 50
-
-# Partitioning
-parted $DISK mklabel gpt
-parted $DISK mkpart ESP fat32 1MiB 512MiB
-parted $DISK set 1 boot on
-parted $DISK mkpart primary 512MiB 100%
+echo 15; echo "Disk bölümlendirme..."
+parted -s $DISK mklabel gpt >>$LOGFILE 2>&1
+parted -s $DISK mkpart ESP fat32 1MiB 512MiB >>$LOGFILE 2>&1
+parted -s $DISK set 1 boot on >>$LOGFILE 2>&1
+parted -s $DISK mkpart primary 512MiB 100% >>$LOGFILE 2>&1
 
 BOOTPART=$(ls ${DISK}* | grep -E "${DISK}p?1$")
 ROOTPART=$(ls ${DISK}* | grep -E "${DISK}p?2$")
 
-mkfs.fat -F32 $BOOTPART
-mkfs.ext4 $ROOTPART
+mkfs.fat -F32 $BOOTPART >>$LOGFILE 2>&1
+mkfs.ext4 $ROOTPART >>$LOGFILE 2>&1
 
-mount $ROOTPART /mnt
-mkdir /mnt/boot
-mount $BOOTPART /mnt/boot
+echo 25; echo "Disk mount ediliyor..."
+mount $ROOTPART /mnt >>$LOGFILE 2>&1
+mkdir -p /mnt/boot
+mount $BOOTPART /mnt/boot >>$LOGFILE 2>&1
 
-# Base system
+echo 40; echo "Mirror listesi güncelleniyor..."
+pacman -Sy --noconfirm >>$LOGFILE 2>&1
+
+echo 55; echo "Temel sistem kuruluyor..."
 pacstrap /mnt base linux linux-firmware networkmanager nano sudo \
   river wlroots mesa intel-media-driver \
-  alacritty waybar rofi greetd plymouth unzip git
+  alacritty waybar rofi greetd plymouth unzip git systemd systemd-boot >>$LOGFILE 2>&1
 
+echo 70; echo "fstab oluşturuluyor..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Chroot işlemleri
-arch-chroot /mnt /bin/bash <<EOF
+echo 85; echo "Chroot işlemleri..."
+arch-chroot /mnt /bin/bash <<EOF >>$LOGFILE 2>&1
 bootctl install
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
@@ -83,6 +90,9 @@ user = "$NEWUSER"
 EOL
 EOF
 
+echo 100; echo "Kurulum tamamlandı!"
+) | whiptail --gauge "Kurulum devam ediyor, lütfen bekleyin..." 20 60 0
+
 umount -R /mnt
-whiptail --title "$TITLE" --msgbox "Kurulum tamamlandı! Arch River hazır." 10 50
+whiptail --title "$TITLE" --msgbox "Kurulum tamamlandı! Arch River hazır.\nLog: $LOGFILE" 10 60
 clear
