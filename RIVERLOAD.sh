02fff7151f9409510d71f47f473fd5b38ccbba5c
erenDO12 <166924100+erenDO12@users.noreply.github.com>
2026-01-16 02:40:41 +0300

    Update RIVERLOAD.sh

--- RIVERLOAD.sh
+++ RIVERLOAD.sh
@@ -3,41 +3,40 @@
 TITLE="Arch Kurulum"
 LOGFILE="/root/kurulum.log"
 
-(
-echo 10; echo "Dil seçimi yapılıyor..."
-
-# 1. Locale (dinamik /etc/locale.gen üzerinden)
+# ------------------------------------------------------------
+# 1. Dil Seçimi
+# ------------------------------------------------------------
 LOCALE=$(whiptail --title "Dil Seçimi" --menu "Kullanmak istediğiniz dili seçin:" 25 80 20 \
 $(grep -E "UTF-8" /etc/locale.gen | sed 's/#//g' | awk '{print $1 " " $1}') \
 3>&1 1>&2 2>&3)
-
 sed -i "s/^#\($LOCALE UTF-8\)/\1/" /etc/locale.gen
 locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
 
-echo 30; echo "Zaman dilimi seçiliyor..."
-
-# 2. Timezone (dinamik timedatectl üzerinden)
+# ------------------------------------------------------------
+# 2. Zaman Dilimi Seçimi
+# ------------------------------------------------------------
 TIMEZONE=$(whiptail --title "Zaman Dilimi Seçimi" --menu "Kullanmak istediğiniz zaman dilimini seçin:" 25 80 20 \
 $(timedatectl list-timezones | awk '{print $1 " " $1}') \
 3>&1 1>&2 2>&3)
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
 
-echo 50; echo "Klavye düzeni seçiliyor..."
-
-# 3. Klavye Düzeni (dinamik localectl üzerinden)
+# ------------------------------------------------------------
+# 3. Klavye Düzeni Seçimi
+# ------------------------------------------------------------
 KEYMAP=$(whiptail --title "Klavye Düzeni Seçimi" --menu "Kullanmak istediğiniz klavye düzenini seçin:" 25 80 20 \
 $(localectl list-keymaps | awk '{print $1 " " $1}') \
 3>&1 1>&2 2>&3)
 echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
 
-echo 70; echo "Ağ bağlantısı seçiliyor..."
-
-# 4. Ağ (iwctl ile dinamik)
-NETTYPE=$(whiptail --title "Bağlantı Türü" --menu "Bağlantı türünü seçin:" 15 60 5 \
+# ------------------------------------------------------------
+# 4. Ağ Bağlantısı
+# ------------------------------------------------------------
+NETTYPE=$(whiptail --title "Ağ Bağlantısı" --menu "Bağlantı türünü seçin:" 15 60 5 \
 "wifi" "Kablosuz (Wi-Fi)" \
 "ethernet" "Kablolu (Ethernet)" \
+"modem" "Mobil Modem (3G/4G)" \
 3>&1 1>&2 2>&3)
 
 if [ "$NETTYPE" = "wifi" ]; then
@@ -50,11 +49,15 @@ if [ "$NETTYPE" = "wifi" ]; then
 elif [ "$NETTYPE" = "ethernet" ]; then
   ip link set eth0 up
   dhcpcd eth0
+elif [ "$NETTYPE" = "modem" ]; then
+  pacman -S --noconfirm modemmanager usb_modeswitch networkmanager
+  systemctl enable ModemManager
+  systemctl enable NetworkManager
 fi
 
-echo 85; echo "Kullanıcı oluşturuluyor..."
-
-# 5. Kullanıcı
+# ------------------------------------------------------------
+# 5. Kullanıcı Oluşturma
+# ------------------------------------------------------------
 USERNAME=$(whiptail --title "Kullanıcı Adı" --inputbox "Yeni kullanıcı adı girin:" 10 60 3>&1 1>&2 2>&3)
 useradd -m -G wheel -s /bin/bash "$USERNAME"
 USERPASS=$(whiptail --title "Kullanıcı Şifresi" --passwordbox "$USERNAME için şifre girin:" 10 60 3>&1 1>&2 2>&3)
@@ -62,13 +65,41 @@ echo "$USERNAME:$USERPASS" | chpasswd
 ROOTPASS=$(whiptail --title "Root Şifresi" --passwordbox "Root için şifre girin:" 10 60 3>&1 1>&2 2>&3)
 echo "root:$ROOTPASS" | chpasswd
 
-echo 90; echo "Masaüstü ortamı seçiliyor..."
-
-# 6. Masaüstü Ortamı (dinamik listeden)
+# ------------------------------------------------------------
+# 6. Masaüstü Ortamı Seçimi
+# ------------------------------------------------------------
 DE=$(whiptail --title "Masaüstü Ortamı Seçimi" --menu "Kurmak istediğiniz masaüstü ortamını seçin:" 20 70 15 \
-$(cat /root/de.list | awk '{print $1 " " $2}') \
+"gnome" "GNOME" \
+"kde" "KDE Plasma" \
+"xfce4" "XFCE" \
+"cinnamon" "Cinnamon" \
+"mate" "MATE" \
+"lxqt" "LXQt" \
+"deepin" "Deepin" \
+"budgie" "Budgie" \
+"hyperland" "Hyperland (Wayland)" \
+"sway" "Sway (Wayland)" \
+"river" "River (Wayland, yay ile)" \
 3>&1 1>&2 2>&3)
 
+# ------------------------------------------------------------
+# 7. Ek Uygulamalar
+# ------------------------------------------------------------
+EXTRA=$(whiptail --title "Ek Uygulamalar" --checklist \
+"Kurmak istediğiniz ek uygulamaları seçin:" 20 70 10 \
+$(pacman -Sl extra | awk '{print $2 " " $2 " OFF"}') \
+3>&1 1>&2 2>&3)
+
+# ------------------------------------------------------------
+# 8. Kurulum İlerleme Barı
+# ------------------------------------------------------------
+(
+echo 10; echo "Dil ayarları uygulanıyor..."
+echo 30; echo "Zaman dilimi ayarlanıyor..."
+echo 50; echo "Klavye düzeni kaydediliyor..."
+echo 70; echo "Ağ yapılandırılıyor..."
+echo 85; echo "Kullanıcı hesapları oluşturuluyor..."
+echo 90; echo "Masaüstü ortamı kuruluyor..."
 case $DE in
   gnome) pacman -S --noconfirm gnome gdm; systemctl enable gdm ;;
   kde) pacman -S --noconfirm plasma kde-applications sddm; systemctl enable sddm ;;
@@ -80,25 +111,17 @@ case $DE in
   budgie) pacman -S --noconfirm budgie-desktop lightdm lightdm-gtk-greeter; systemctl enable lightdm ;;
   hyperland) pacman -S --noconfirm hyperland ;;
   sway) pacman -S --noconfirm sway ;;
+  river) pacman -S --noconfirm yay && sudo -u "$USERNAME" yay -S --noconfirm river ;;
 esac
-
-echo 95; echo "Ek uygulamalar seçiliyor..."
-
-# 7. Ek Uygulamalar (dinamik pacman üzerinden)
-EXTRA=$(whiptail --title "Ek Uygulamalar" --checklist \
-"Kurmak istediğiniz ek uygulamaları seçin:" 20 70 10 \
-$(pacman -Sl extra | awk '{print $2 " " $2 " OFF"}') \
-3>&1 1>&2 2>&3)
-
+echo 95; echo "Ek uygulamalar yükleniyor..."
 for app in $EXTRA; do
   pacman -S --noconfirm $(echo $app | tr -d '"')
 done
-
 echo 100; echo "Kurulum tamamlandı!"
-) | whiptail --gauge "Kurulum devam ediyor, lütfen bekleyin..." 20 60 0
+) | whiptail --title "Kurulum İlerleme Durumu" --gauge "Kurulum devam ediyor, lütfen bekleyin..." 20 60 0
 
 # ------------------------------------------------------------
-# 8. Kurulum Sonrası İşlemler
+# 9. Kurulum Sonrası
 # ------------------------------------------------------------
 umount -R /mnt || true
 whiptail --title "$TITLE" --msgbox "Kurulum tamamlandı! Arch Linux + $DE hazır.\nSeçilen ek uygulamalar da kuruldu.\nLog: $LOGFILE" 10 60
